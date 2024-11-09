eec122b344f31d77c546dd833a6d282719cf0514
shahzodotaboyev <shoxzodbek15@gmail.com>
2024-11-09 10:21:35 +0300

    menu

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
+let menu = document.querySelector('.headerMenu')
 
 let headerMenu = () => {
-
+    menu.classList.add('headerMenu0')
+}
+let deleteMenu = () => {
+    menu.classList.remove('headerMenu0')
 }
